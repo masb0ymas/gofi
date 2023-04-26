e125be25599be06e0bf93dd5086ee6798a024e7e
masb0ymas <n.fajri@outlook.com>
2023-04-26 12:04:25 +0700

    fix: base entity

--- src/database/entities/base.entity.go
+++ src/database/entities/base.entity.go
@@ -1,15 +1,15 @@
 package entities
 
 import (
+	"database/sql"
 	"time"
 
 	"github.com/google/uuid"
-	"github.com/lib/pq"
 )
 
 type BaseEntity struct {
-	Id        uuid.UUID   `json:"id" db:"id"`
-	CreatedAt time.Time   `json:"createdAt" db:"created_at"`
-	UpdatedAt time.Time   `json:"updatedAt" db:"updated_at"`
-	DeletedAt pq.NullTime `json:"deletedAt" db:"deleted_at"`
+	Id        uuid.UUID    `json:"id" db:"id"`
+	CreatedAt time.Time    `json:"createdAt" db:"created_at"`
+	UpdatedAt time.Time    `json:"updatedAt" db:"updated_at"`
+	DeletedAt sql.NullTime `json:"deletedAt" db:"deleted_at"`
 }
