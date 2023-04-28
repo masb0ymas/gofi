836636c3605c09540be27c45a420b43023d1597e
masb0ymas <n.fajri@outlook.com>
2023-04-28 21:58:45 +0700

    feat: add endpoint user

--- src/database/entities/user.entity.go
+++ src/database/entities/user.entity.go
@@ -7,13 +7,25 @@ import (
 
 type UserEntity struct {
 	BaseEntity
-	Fullname    string      `json:"fullname" db:"fullname"`
-	Email       string      `json:"email" db:"email"`
-	Password    string      `json:"password" db:"password"`
-	Phone       null.String `json:"phone" db:"phone"`
-	TokenVerify null.String `json:"tokenVerify" db:"token_verify"`
-	Address     null.String `json:"address" db:"address"`
-	IsActive    bool        `json:"isActive" db:"is_active"`
-	IsBlocked   bool        `json:"isBlocked" db:"is_blocked"`
-	RoleId      uuid.UUID   `json:"RoleId" db:"role_id"`
+	Fullname    string       `json:"fullname" db:"fullname"`
+	Email       string       `json:"email" db:"email"`
+	Password    string       `json:"password" db:"password"`
+	Phone       null.String  `json:"phone" db:"phone"`
+	TokenVerify null.String  `json:"tokenVerify" db:"token_verify"`
+	Address     null.String  `json:"address" db:"address"`
+	IsActive    bool         `json:"isActive" db:"is_active"`
+	IsBlocked   bool         `json:"isBlocked" db:"is_blocked"`
+	RoleId      uuid.UUID    `json:"RoleId" db:"role_id"`
+	Role        []RoleEntity `json:"Role"`
+}
+
+type UserAllEntity struct {
+	BaseEntity
+	Fullname  string      `json:"fullname" db:"fullname"`
+	Email     string      `json:"email" db:"email"`
+	Phone     null.String `json:"phone" db:"phone"`
+	Address   null.String `json:"address" db:"address"`
+	IsActive  bool        `json:"isActive" db:"is_active"`
+	IsBlocked bool        `json:"isBlocked" db:"is_blocked"`
+	RoleId    uuid.UUID   `json:"RoleId" db:"role_id"`
 }
