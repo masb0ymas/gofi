6c8a919bd203db7402ceb06bfdfd80d2cfa821d3
masb0ymas <n.fajri@outlook.com>
2025-11-16 13:33:21 +0700

    feat: add sessions table

---
+++ migrations/000006_create-session-table.down.sql
@@ -0,0 +1,7 @@
+DROP INDEX IF EXISTS idx_sessions_id;
+DROP INDEX IF EXISTS idx_sessions_created_at;
+DROP INDEX IF EXISTS idx_sessions_updated_at;
+DROP INDEX IF EXISTS idx_sessions_user_id;
+DROP INDEX IF EXISTS idx_sessions_token;
+
+DROP TABLE IF EXISTS public."sessions";
---
+++ migrations/000006_create-session-table.up.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS "sessions" (
+  "id" UUID PRIMARY KEY NOT NULL DEFAULT uuidv7(),
+  "created_at" TIMESTAMP DEFAULT now(),
+  "updated_at" TIMESTAMP DEFAULT now(),
+  "user_id" UUID NOT NULL,
+  "token" VARCHAR NOT NULL
+);
+
+CREATE INDEX IF NOT EXISTS idx_sessions_id ON "sessions" ("id");
+CREATE INDEX IF NOT EXISTS idx_sessions_created_at ON "sessions" ("created_at");
+CREATE INDEX IF NOT EXISTS idx_sessions_updated_at ON "sessions" ("updated_at");
+CREATE INDEX IF NOT EXISTS idx_sessions_user_id ON "sessions" ("user_id");
+CREATE INDEX IF NOT EXISTS idx_sessions_token ON "sessions" ("token");
