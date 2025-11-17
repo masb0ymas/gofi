ad5587e0cda0cbce1417102200a1b45ee517ad9c
masb0ymas <n.fajri@outlook.com>
2025-11-17 12:08:53 +0700

    fix: update session migrations

--- migrations/000006_create-session-table.down.sql
+++ migrations/000006_create-session-table.down.sql
@@ -3,5 +3,6 @@ DROP INDEX IF EXISTS idx_sessions_created_at;
 DROP INDEX IF EXISTS idx_sessions_updated_at;
 DROP INDEX IF EXISTS idx_sessions_user_id;
 DROP INDEX IF EXISTS idx_sessions_token;
+DROP INDEX IF EXISTS idx_sessions_expires_at;
 
 DROP TABLE IF EXISTS public."sessions";
--- migrations/000006_create-session-table.up.sql
+++ migrations/000006_create-session-table.up.sql
@@ -3,7 +3,10 @@ CREATE TABLE IF NOT EXISTS "sessions" (
   "created_at" TIMESTAMP DEFAULT now(),
   "updated_at" TIMESTAMP DEFAULT now(),
   "user_id" UUID NOT NULL,
-  "token" VARCHAR NOT NULL
+  "token" VARCHAR NOT NULL,
+  "expires_at" TIMESTAMP NOT NULL,
+  "ip_address" VARCHAR NOT NULL,
+  "user_agent" TEXT NOT NULL
 );
 
 CREATE INDEX IF NOT EXISTS idx_sessions_id ON "sessions" ("id");
@@ -11,3 +14,4 @@ CREATE INDEX IF NOT EXISTS idx_sessions_created_at ON "sessions" ("created_at");
 CREATE INDEX IF NOT EXISTS idx_sessions_updated_at ON "sessions" ("updated_at");
 CREATE INDEX IF NOT EXISTS idx_sessions_user_id ON "sessions" ("user_id");
 CREATE INDEX IF NOT EXISTS idx_sessions_token ON "sessions" ("token");
+CREATE INDEX IF NOT EXISTS idx_sessions_expires_at ON "sessions" ("expires_at");
