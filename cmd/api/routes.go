f0127240513189bece913a175e6fbfe51aa91884
masb0ymas <n.fajri@outlook.com>
2025-11-25 18:10:47 +0700

    fix: update routes

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -28,32 +28,34 @@ func routes(r *fiber.App, app *app.Application) {
 	authRoutes.Get("/verify-session", m.Authorization(), h.Auth.VerifySession)
 	authRoutes.Post("/sign-out", m.Authorization(), h.Auth.SignOut)
 
+	adminOnly := []string{constant.RoleAdmin}
+
 	sessionRoutes := r.Group("/v1/sessions")
-	sessionRoutes.Use(m.Authorization(), m.PermissionAccess([]string{constant.RoleAdmin}))
+	sessionRoutes.Use(m.Authorization(), m.PermissionAccess(adminOnly))
 	sessionRoutes.Get("", h.Session.Index)
 
 	roleRoutes := r.Group("/v1/roles")
 	roleRoutes.Use(m.Authorization())
 	roleRoutes.Get("", h.Role.Index)
 	roleRoutes.Get("/:roleID", h.Role.Show)
-	roleRoutes.Post("", m.PermissionAccess([]string{constant.RoleAdmin}), h.Role.Create)
-	roleRoutes.Put("/:roleID", m.PermissionAccess([]string{constant.RoleAdmin}), h.Role.Update)
-	roleRoutes.Delete("/:roleID", m.PermissionAccess([]string{constant.RoleAdmin}), h.Role.Delete)
-	roleRoutes.Delete("/:roleID/soft-delete", m.PermissionAccess([]string{constant.RoleAdmin}), h.Role.SoftDelete)
-	roleRoutes.Patch("/:roleID/restore", m.PermissionAccess([]string{constant.RoleAdmin}), h.Role.Restore)
+	roleRoutes.Post("", m.PermissionAccess(adminOnly), h.Role.Create)
+	roleRoutes.Put("/:roleID", m.PermissionAccess(adminOnly), h.Role.Update)
+	roleRoutes.Delete("/:roleID", m.PermissionAccess(adminOnly), h.Role.Delete)
+	roleRoutes.Delete("/:roleID/soft-delete", m.PermissionAccess(adminOnly), h.Role.SoftDelete)
+	roleRoutes.Patch("/:roleID/restore", m.PermissionAccess(adminOnly), h.Role.Restore)
 
 	userRoutes := r.Group("/v1/users")
 	userRoutes.Use(m.Authorization())
 	userRoutes.Get("", h.User.Index)
 	userRoutes.Get("/:userID", h.User.Show)
-	userRoutes.Post("", m.PermissionAccess([]string{constant.RoleAdmin}), h.User.Create)
-	userRoutes.Put("/:userID", m.PermissionAccess([]string{constant.RoleAdmin}), h.User.Update)
-	userRoutes.Delete("/:userID", m.PermissionAccess([]string{constant.RoleAdmin}), h.User.Delete)
-	userRoutes.Delete("/:userID/soft-delete", m.PermissionAccess([]string{constant.RoleAdmin}), h.User.SoftDelete)
-	userRoutes.Patch("/:userID/restore", m.PermissionAccess([]string{constant.RoleAdmin}), h.User.Restore)
+	userRoutes.Post("", m.PermissionAccess(adminOnly), h.User.Create)
+	userRoutes.Put("/:userID", m.PermissionAccess(adminOnly), h.User.Update)
+	userRoutes.Delete("/:userID", m.PermissionAccess(adminOnly), h.User.Delete)
+	userRoutes.Delete("/:userID/soft-delete", m.PermissionAccess(adminOnly), h.User.SoftDelete)
+	userRoutes.Patch("/:userID/restore", m.PermissionAccess(adminOnly), h.User.Restore)
 
 	// Not found handler
-	r.Get("*", func(c *fiber.Ctx) error {
+	r.Use("*", func(c *fiber.Ctx) error {
 		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
 			"message": "Sorry, HTTP resource you are looking for was not found.",
 		})
