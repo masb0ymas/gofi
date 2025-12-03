69e8a41838227205c56d955f6fe393aa379e7fb1
masb0ymas <n.fajri@outlook.com>
2025-12-03 16:20:55 +0700

    fix: update user oauth migrations

--- migrations/000008_create-user-oauth-table.down.sql
+++ migrations/000008_create-user-oauth-table.down.sql
@@ -1,7 +1,7 @@
-DROP INDEX IF EXISTS idx_users_oauth_id;
-DROP INDEX IF EXISTS idx_users_oauth_user_id;
-DROP INDEX IF EXISTS idx_users_oauth_provider;
-DROP INDEX IF EXISTS idx_users_oauth_access_token;
-DROP INDEX IF EXISTS idx_users_oauth_expires_at;
+DROP INDEX IF EXISTS idx_user_oauths_id;
+DROP INDEX IF EXISTS idx_user_oauths_user_id;
+DROP INDEX IF EXISTS idx_user_oauths_provider;
+DROP INDEX IF EXISTS idx_user_oauths_access_token;
+DROP INDEX IF EXISTS idx_user_oauths_expires_at;
 
-DROP TABLE IF EXISTS public."users_oauth";
+DROP TABLE IF EXISTS public."user_oauths";
--- migrations/000008_create-user-oauth-table.up.sql
+++ migrations/000008_create-user-oauth-table.up.sql
@@ -1,16 +1,16 @@
-CREATE TABLE IF NOT EXISTS "users_oauth" (
+CREATE TABLE IF NOT EXISTS "user_oauths" (
   "id" UUID PRIMARY KEY NOT NULL DEFAULT uuidv7(),
   "user_id" UUID NOT NULL,
   "provider" VARCHAR NOT NULL,
   "access_token" TEXT NOT NULL,
   "refresh_token" TEXT,
-  "expires_at" TIMESTAMP NOT NULL,
+  "expires_at" TIMESTAMP NOT NULL
 );
 
-CREATE INDEX IF NOT EXISTS idx_users_oauth_id ON "users_oauth" ("id");
-CREATE INDEX IF NOT EXISTS idx_users_oauth_user_id ON "users_oauth" ("user_id");
-CREATE INDEX IF NOT EXISTS idx_users_oauth_provider ON "users_oauth" ("provider");
-CREATE INDEX IF NOT EXISTS idx_users_oauth_access_token ON "users_oauth" ("access_token");
-CREATE INDEX IF NOT EXISTS idx_users_oauth_expires_at ON "users_oauth" ("expires_at");
+CREATE INDEX IF NOT EXISTS idx_user_oauths_id ON "user_oauths" ("id");
+CREATE INDEX IF NOT EXISTS idx_user_oauths_user_id ON "user_oauths" ("user_id");
+CREATE INDEX IF NOT EXISTS idx_user_oauths_provider ON "user_oauths" ("provider");
+CREATE INDEX IF NOT EXISTS idx_user_oauths_access_token ON "user_oauths" ("access_token");
+CREATE INDEX IF NOT EXISTS idx_user_oauths_expires_at ON "user_oauths" ("expires_at");
 
-ALTER TABLE "users_oauth" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id") ON DELETE CASCADE;
+ALTER TABLE "user_oauths" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id") ON DELETE CASCADE;
